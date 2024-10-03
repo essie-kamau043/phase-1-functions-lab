e0f2e9ff6179deb87b84d2f404e5a59cffefc128
essie-kamau043 <essie.k004@gmail.com>
2024-10-03 12:28:57 +0300

    add index.js

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(location){
+  const hqLocation=42;
+  return Math.abs(location -hqLocation);
+}
+
+function distanceFromHqInFeet(location){
+  const blocks=distanceFromHqInBlocks(location)
+  return blocks * 264
+}
+
+
+  function distanceTravelledInFeet(uptown, dowmtown){
+return Math.abs(uptown - dowmtown) * 264;
+
+  }
+
+  function calculatesFarePrice(uptown, downtown){
+    const distanceTravelledInFeet=Math.abs(uptown - downtown) *264;
+    
+    if(distanceTravelledInFeet<=400){
+    return 0;
+  }
+   else if(distanceTravelledInFeet>400 && distanceTravelledInFeet<=2000){
+    return (distanceTravelledInFeet-400) * 0.02;
+
+   }
+   else if(distanceTravelledInFeet>2000 && distanceTravelledInFeet<=2500){
+    return 25;
+
+   }
+   else{
+    return 'cannot travel that far';
+   }
+}
